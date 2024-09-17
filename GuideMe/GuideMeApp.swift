6c74168ab64e35866ff7e18eaf64f9e58e4b729c
elliotrap <elliotmrapp@gmail.com>
2024-09-16 20:42:14 -0500

    Initial Commit

---
+++ GuideMe/GuideMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuideMeApp.swift
+//  GuideMe
+//
+//  Created by Elliot Rapp on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct GuideMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
